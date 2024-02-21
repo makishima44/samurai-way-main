dac4aae162e9f8068b5216ba770cde30cd805be5
Artur <metallist15431543@gmail.com>
2024-02-21 19:28:42 +0300

    Добавлена верстка блока Dialogs

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,5 +1,31 @@
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
 
 export const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + " " + s.active}>
+          <NavLink to="/dialogs/1">Dimych</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/2">Andrey</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/3">Sasha</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/4">Viktor</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/5">Valera</NavLink>
+         </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How is your it-kamasutra</div>
+        <div className={s.message}>Yo</div>
+      </div>
+    </div>
+  );
 };
